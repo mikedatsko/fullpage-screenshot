bd890795a62554afaeffdc639111abf43f0d24a7
Mike Datsko <mike@datsko.it>
2018-11-13 23:18:30 +0200

    deploy fixes

--- routes/screenshot.js
+++ routes/screenshot.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 
 function getBrowser(width, height, isMobile) {
   return puppeteer.connect({
-    browserWSEndpoint: 'ws://browser:3001',
+    browserWSEndpoint: 'ws://browser:3000',
     defaultViewport: {
       width: width,
       height: height,
